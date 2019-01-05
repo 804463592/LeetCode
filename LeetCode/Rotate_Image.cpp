0974d4c0780fe35d2a84c702242975f2b6f51c22
804463592 <804463592@qq.com>
2019-01-05 21:57:18 +0800

    完成旋转图像LeetCode

--- LeetCode/Rotate_Image.cpp
+++ LeetCode/Rotate_Image.cpp
@@ -16,26 +16,48 @@ class Solution {
 public:
 	void rotate(vector<vector<int>>& matrix)
 	{
-		//这道题的编程思路,就是先水平翻转,然后垂直翻转
+		//这道题的思路,先上下镜像对称(垂直翻转),再转置
 
-		//先水平反转
-		for (vector<vector<int>>::iterator iter1 = matrix.begin(); iter1 != matrix.end(); iter1++)
+		//垂直翻转
+		for (size_t i = 0; i < matrix.size()/2; i++)
 		{
-			for (int i = 0; i < int(iter1->size()) / 2; i++)
-			{
+			for (size_t j = 0; j < matrix[i].size(); j++) {
+
 				int temp = 0;
-				temp = (*iter1)[0 + i];
-				(*iter1)[0 + i] = (*iter1)[iter1->size() - i-1];  //减一,不然索引溢出
-				(*iter1)[iter1->size() - i-1] = temp;
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[matrix.size() - i - 1][j];
+				matrix[matrix.size() - i - 1][j] = temp;
+			}
+
+		}
+		//转置
+		for (size_t i = 0; i < matrix.size(); i++)
+		{
+			for (size_t j = i; j < matrix[i].size(); j++) {  //注意j不要从0开始,否则会转置两次,相当于没有转置
+
+				int temp = 0;
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
 
 			}
+
 		}
 
-		//然后垂直旋转
 
 
+		////水平翻转
+		//for (vector<vector<int>>::iterator iter1 = matrix.begin(); iter1 != matrix.end(); iter1++)
+		//{
+		//	for (int i = 0; i < int(iter1->size()) / 2; i++)
+		//	{
+		//		int temp = 0;
+		//		temp = (*iter1)[0 + i];
+		//		(*iter1)[0 + i] = (*iter1)[iter1->size() - i-1];  //减一,不然索引溢出
+		//		(*iter1)[iter1->size() - i-1] = temp;
 
-
+		//	}
+		//}
 
 	}
 };
@@ -44,8 +66,8 @@ public:
 int main() {
 
 	vector<int> ivec1 = { 1,2,3 };
-	vector<int> ivec2 = { 4,5,6, };
-	vector<int> ivec3 = { 7,8,9, };
+	vector<int> ivec2 = { 4,5,6 };
+	vector<int> ivec3 = { 7,8,9 };
 
 	vector<vector<int>> ivec;
 	ivec.push_back(ivec1);
@@ -61,9 +83,9 @@ int main() {
 		for (vector<int>::iterator iter = it->begin(); iter != it->end(); iter++)
 		{
 
-			cout << *iter<<"  ";
+			cout << *iter << "  ";
 		}
-		cout << endl<<endl;
+		cout << endl << endl;
 
 	}
 
