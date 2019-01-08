7ab0c7cd87c1f9819a6f137ec9695ed158da0778
804463592 <804463592@qq.com>
2019-01-08 22:32:53 +0800

    开始LeetCode题目:trapping rain water ;中文名"接雨水"

--- LeetCode/countAndSay.cpp
+++ LeetCode/countAndSay.cpp
@@ -1,3 +1,23 @@
+
+/*
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+*/
+
 #include <string>
 #include <iostream>
 using namespace std;
@@ -7,13 +27,12 @@ public:
 	string readNextStr(string s) {
 		int n = 1;
 		int i = 0;
-		string res_str ="";
-		while(i < s.size())   //while
+		string res_str = "";
+		while (i < s.size())
 		{
 			if (s[i] == s[i + 1])
 			{
-
-				while (((i+1)<s.size()) && s[i] == s[i + 1])
+				while (((i + 1) < s.size()) && s[i] == s[i + 1])
 				{
 					n++;
 					i++;
@@ -22,7 +41,7 @@ public:
 				n = 1;
 				res_str.push_back((s[i]));
 				i++;
-			
+
 			}
 			else {
 
@@ -41,30 +60,27 @@ public:
 
 
 	string countAndSay(int n) {
-		string str ="1";
-		for (int i = 0; i < n-1; i++)
+		string str = "1";
+		for (int i = 0; i < n - 1; i++)
 		{
 			str = readNextStr(str);
-		
 		}
 
 		return str;
 	}
 };
 
-int main() {
-
-	Solution solu;
-	//string res = solu.readNextStr(to_string(13112221));
-	string res = solu.countAndSay(5);
-	cout << res<<endl;
-
-	//string str;
-	//str.push_back(res[2]);
-	//cout << str;
-	//cout << res[0];
-
-	getchar();
-	return 0;
-}
+//int main() {
+//
+//	Solution solu;
+//	//string res = solu.readNextStr(to_string(13112221));
+//	int n = 0;
+//	cout << "请输入一个正整数:" << endl;
+//	cin >> n;
+//	string res = solu.countAndSay(n);
+//	cout << "报数的输出结果为:" << res << endl;
+//
+//	system("pause");
+//	return 0;
+//}
 
