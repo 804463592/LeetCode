654853f982c32378b2687dc033a12b16ca06e84c
804463592 <804463592@qq.com>
2019-01-08 13:31:12 +0800

    完成countAndSay题目,中文题目名字叫"报数"

--- LeetCode/countAndSay.cpp
+++ LeetCode/countAndSay.cpp
@@ -4,14 +4,49 @@ using namespace std;
 
 class Solution {
 public:
-	string countAndSay(int n) {
-		string str ="1";
-		for (int i = 0; i < n; i++)
+	string readNextStr(string s) {
+		int n = 1;
+		int i = 0;
+		string res_str ="";
+		while(i < s.size())   //while
 		{
-			str.append(to_string(1));
+			if (s[i] == s[i + 1])
+			{
+
+				while (((i+1)<s.size()) && s[i] == s[i + 1])
+				{
+					n++;
+					i++;
+				}
+				res_str.append(to_string(n));
+				n = 1;
+				res_str.push_back((s[i]));
+				i++;
+			
+			}
+			else {
+
+				res_str.append(to_string(n));
+				n = 1;
+				res_str.push_back((s[i]));
+				i++;
+
+			}
+
 		}
 
+		return res_str;
+
+	}
+
+
+	string countAndSay(int n) {
+		string str ="1";
+		for (int i = 0; i < n-1; i++)
+		{
+			str = readNextStr(str);
 		
+		}
 
 		return str;
 	}
@@ -20,8 +55,14 @@ public:
 int main() {
 
 	Solution solu;
-	string res =solu.countAndSay(2);
+	//string res = solu.readNextStr(to_string(13112221));
+	string res = solu.countAndSay(5);
 	cout << res<<endl;
+
+	//string str;
+	//str.push_back(res[2]);
+	//cout << str;
+	//cout << res[0];
 
 	getchar();
 	return 0;
