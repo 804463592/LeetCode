d064e22ef15306a24fb41cbafc0588318a887c32
804463592 <804463592@qq.com>
2019-04-20 16:32:35 +0800

    注释

--- LeetCode/Minimum_Size_Subarray_Sum.cpp
+++ LeetCode/Minimum_Size_Subarray_Sum.cpp
@@ -12,28 +12,56 @@
 
 */
 
-
 #include<iostream>
 #include<algorithm>
 #include<vector>
 using namespace std;
 class Solution {
 public:
+
 	int minSubArrayLen(int s, vector<int>& nums) {
-		int ans = 0;
+		
+		int n_size = nums.size();
+		int ans = n_size;
+
+		if (n_size == 0)return 0;
+		if (n_size == 1)return nums[0] > s ? 1 : 0;
+		
+		for (int i = 0; i < n_size; i++)
+		{
+			int tmp_sum = nums[i];
+			//子序列只需要一个长度
+			if (nums[i] >= s) return 1;
+			//子序列需要两个，甚至多个长度
+			int j = i + 1;
+			if (j >= n_size)break;
+
+			while (tmp_sum + nums[j] < s)
+			{
+				tmp_sum += nums[j];
+				if(j<n_size)
+				j++;
+				else break;
+			}
+	
+			ans = j - i + 1< ans ? j - i + 1:ans;
 
 
-		return 0;
+		}
+
+
+
+		return ans;
 	}
 };
 
 int main()
 {
 	Solution solu;
-	vector<int> ivec = { 2,3,1,2,4,3 };
+	vector<int> ivec = { 2,3,1,2,4,3,2 };
 
 
-	solu.minSubArrayLen(7,ivec);
+	cout << solu.minSubArrayLen(7, ivec) << endl;
 
 	system("pause");
 	return 0;
